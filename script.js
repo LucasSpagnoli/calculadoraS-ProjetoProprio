cfca043d85aa3f347493bc76f641ce7711e7ac0e
LucasSpagnoli <lm.spanholi@gmail.com>
2025-01-29 10:28:59 -0300

    quando tu entende a lógica fica mais fácil. Tá pronto já praticamente, só vou tentar melhorar o código e adicionar algumas outras funções pra entretenimento mesmo.

--- script.js
+++ script.js
@@ -12,15 +12,29 @@ function num(x){
     }
     check++
 }
-function soma(){
+function operar(x){
     var visor = Number(res.value)
     nums.push(visor)
     res.value=''
-    operacao = 'soma'
-    window.alert(nums)
+    if (x==1){
+        operacao = 'soma'
+    } else if (x==2){
+        operacao = 'fatorial'
+        igual()
+    } else if (x==3){
+        operacao = 'subtração'
+    } else if (x==4){
+        operacao = 'potência'
+    } else if (x==5){
+        operacao = 'multiplicar'
+    } else if (x==6){
+        operacao = 'raiz'
+        igual()
+    }
 }
 function igual(){
-    if (check!=0){
+
+    if (check!=0 && operacao!='fatorial' && operacao!='raiz'){
     var visor = Number(res.value)
     nums.push(visor)
     if (operacao == 'soma'){
@@ -29,10 +43,51 @@ function igual(){
         }
         res.value=final
         nums.length=0
-    } else {
+    } else if (operacao == 'subtração'){
+        final = nums[0]
+        for (let i = 1; i < nums.length; i++) {
+            final -= nums[i]
+        }
+        res.value=final
+        nums.length=0
+    } else if (operacao == 'multiplicar'){
+        final = 1
+        for (let i = 0; i < nums.length; i++) {
+            final *= nums[i]
+        }
+        res.value=final
+        nums.length=0
+    } else if (operacao == 'potência'){
+        final=nums[0]
+            final**=nums[1]
+        res.value=final
+        nums.length=0
+    } else if (operacao == 'raiz'){
+        final=nums[0]
+            final**=nums[1]
+        res.value=final
+        nums.length=0
+    }else {
         window.alert('nah')
     }
-    check = 0
+    } else if (operacao=='fatorial'){
+        var visor = Number(res.value)
+        nums.push(visor)
+        let i=1
+        final=nums[0]
+        while (i<nums[0]) {
+            final*=nums[0]-i
+            i++
+        }
+        res.value=final
+        nums.length=0
+    } else if (operacao=='raiz'){
+        var visor = Number(res.value)
+        nums.push(visor)
+        final=Math.sqrt(nums[0])
+        res.value=final
+        nums.length=0
     }
+    check = 0
     final=0
 }
